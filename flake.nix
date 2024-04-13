a4b47eaf747f7b778d7365ef41a9078d48e46c42
Matthew Dargan <matthewdargan57@gmail.com>
2024-04-13 10:55:07 -0500

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs:
+    inputs.parts.lib.mkFlake {inherit inputs;} {
+      perSystem = {pkgs, ...}: {
+        packages = {
+          go = pkgs.go.overrideAttrs (_: rec {
+            src = pkgs.fetchurl {
+              hash = "sha256-N06oKyiexzjpaCZ8rFnH1f8YD5SSJQJUeEsgROkN9ak=";
+              url = "https://go.dev/dl/go${version}.src.tar.gz";
+            };
+            version = "1.22.2";
+          });
+          golangci-lint = pkgs.writeShellApplication {
+            name = "golangci-lint";
+            runtimeInputs = [pkgs.go pkgs.golangci-lint];
+            text = ''exec golangci-lint "$@"'';
+          };
+        };
+      };
+      systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
+    };
+}
