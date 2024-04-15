b0fcb6f57cf95f6e7ad972605e0162474a64282e
Matthew Dargan <matthewdargan57@gmail.com>
2024-04-15 08:06:44 -0500

    feat: add pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -2,10 +2,16 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     parts.url = "github:hercules-ci/flake-parts";
+    pre-commit-hooks = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:cachix/pre-commit-hooks.nix";
+    };
   };
   outputs = inputs:
     inputs.parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.pre-commit-hooks.flakeModule];
       perSystem = {
+        config,
         pkgs,
         self',
         ...
@@ -13,6 +19,9 @@
         inherit (self'.packages) go;
         inherit (self'.legacyPackages) buildGoModule;
       in {
+        devShells.default = pkgs.mkShell {
+          shellHook = "${config.pre-commit.installationScript}";
+        };
         legacyPackages.buildGoModule = pkgs.callPackage "${inputs.nixpkgs}/pkgs/build-support/go/module.nix" {
           inherit go;
         };
@@ -31,6 +40,16 @@
           };
           gopls = pkgs.callPackage "${inputs.nixpkgs}/pkgs/development/tools/language-servers/gopls" {
             inherit buildGoModule;
+          };
+        };
+        pre-commit = {
+          settings = {
+            hooks = {
+              alejandra.enable = true;
+              deadnix.enable = true;
+              statix.enable = true;
+            };
+            src = ./.;
           };
         };
       };
